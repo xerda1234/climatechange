cafaa2b4b6bda113340f33d8212bcfcda971529d
Glitch (climatechange) <none>
2017-12-03 19:53:11 +0000

    🐟👩‍❤️‍💋‍👩 Updated with Glitch

---
+++ science.js
@@ -0,0 +1,5 @@
+console.log(100*100)
+function person(name, age) {
+  console.log("I hate" + name + "because he/she sux and is " + age + "years old");
+  
+}
---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+const main = document.getElementById('myTopnav');
+
+var foo =2*20;
+
+foo = foo * foo;
+
+console.log(foo*foo);
+
+var ourButton = document.getElementById("button");
+ourButton.addEventListener("click", function(){
+  ourButton.style.color = "#FFF000";
+  alert
+  ("Climate change is bad")
+  
+});
+document.getElementById("lol").addEventListener("mouseover",x);
+function x()  {
+   console.log("cool")
+}
+
+    $(document).ready(function(){
+      $('.slider').slider();
+    });
+
+
